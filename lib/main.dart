f2a2ec1f136761e580cc5793d966bc9ed1293434
Кирилл Долматов <breakitedkirill@mail.ru>
2023-04-27 15:12:53 +1000

    Create service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ma_for_feip/app.dart';
+import 'package:ma_for_feip/service_locator/app_locator.dart';
 
-void main() {
+void main() async {
+  await AppLocator.instance.init();
+  
   runApp(const ProviderScope(child: App()));
 }
