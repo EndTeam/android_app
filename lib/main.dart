31d6496bc5fbcf39a2a34677ebc6ac293c57c82f
Кирилл Долматов <breakitedkirill@mail.ru>
2023-04-27 16:39:31 +1000

    Add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:ma_for_feip/app.dart';
+import 'package:ma_for_feip/firebase_options.dart';
 import 'package:ma_for_feip/service_locator/app_locator.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   await AppLocator.instance.init();
-  
+
   runApp(const ProviderScope(child: App()));
 }
