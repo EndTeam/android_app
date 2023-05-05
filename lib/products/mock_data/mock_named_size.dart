c2a19b225b2bd02ae442590cd73836002e254de4
Kirill Dolmatov <dolmatov.kd@dns-shop.ru>
2023-05-05 19:00:25 +1000

    Separate mock generate to classes

---
+++ lib/products/mock_data/mock_named_size.dart
@@ -0,0 +1,19 @@
+import 'package:faker/faker.dart';
+import 'package:ma_for_feip/products/models_dto/named_size/named_size.dart';
+
+class MockNamedSize {
+  static const _sizeList = ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
+
+  List<NamedSizeDTO> genSizes([int min = 1, int max = 7]) {
+    assert(max <= _sizeList.length);
+    assert(min <= max);
+    assert(min >= 1);
+
+    final newList = List.from(_sizeList, growable: false)..shuffle();
+    return faker.randomGenerator.amount(
+      (i) => NamedSizeDTO(label: newList[i]),
+      max,
+      min: min,
+    );
+  }
+}
